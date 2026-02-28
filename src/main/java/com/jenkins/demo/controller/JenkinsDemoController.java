6c72f9a53728a6268412901d6029efef503d403d
Prashant Suryawanshi <prashant1993ds@gmail.com>
2026-02-28 13:35:12 +0530

    added welcome rest endpoint in jenkins cicd demo

---
+++ src/main/java/com/jenkins/demo/controller/JenkinsDemoController.java
@@ -0,0 +1,15 @@
+package com.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsDemoController {
+
+    @GetMapping("/welcome")
+    public String welcomeJenkins(){
+        return "Welcome to Jenkins CI/CD flow Demo";
+    }
+}
